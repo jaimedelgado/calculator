9a7dbdf25c292b06b245729a8ff290a95885ef69
jdelgado <jdelgado@paradigmadigital.com>
2020-06-24 00:04:48 +0200

    Solucionar bug con la resta y crear test en el mapper

--- src/main/java/com/sanitas/calculator/controller/mapper/OperationsMapper.java
+++ src/main/java/com/sanitas/calculator/controller/mapper/OperationsMapper.java
@@ -3,12 +3,14 @@ package com.sanitas.calculator.controller.mapper;
 import com.sanitas.calculator.controller.request.get.Operator;
 import com.sanitas.calculator.model.Addition;
 import com.sanitas.calculator.model.Operation;
+import com.sanitas.calculator.model.Subtraction;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OperationsMapper {
 
     public Operation toDTO(double operand1, Operator operator, double operand2) {
-        return operator.equals(Operator.ADD) ? new Addition(operand1, operand2) : null;
+        return operator.equals(Operator.ADD) ? new Addition(operand1, operand2)
+            : operator.equals(Operator.SUB) ? new Subtraction(operand1, operand2) : null;
     }
 }
--- src/test/java/com/sanitas/calculator/controller/mapper/OperationsMapperTest.java
+++ src/test/java/com/sanitas/calculator/controller/mapper/OperationsMapperTest.java
@@ -3,6 +3,7 @@ package com.sanitas.calculator.controller.mapper;
 import com.sanitas.calculator.controller.request.get.Operator;
 import com.sanitas.calculator.model.Addition;
 import com.sanitas.calculator.model.Operation;
+import com.sanitas.calculator.model.Subtraction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -17,12 +18,22 @@ public class OperationsMapperTest {
     }
 
     @Test
-    public void restToDTOTest() {
+    public void additionOperationDTOTest() {
         double operand2 = 2;
         double operand1 = 1;
         Operation operation = operationsMapper.toDTO(operand1, Operator.ADD, operand2);
         Assertions.assertTrue(operation instanceof Addition);
         Assertions.assertEquals(((Addition) operation).getOperand1(), operand1);
         Assertions.assertEquals(((Addition) operation).getOperand2(), operand2);
+    }
+
+    @Test
+    public void subtractionOperationDTOTest() {
+        double operand2 = 2;
+        double operand1 = 1;
+        Operation operation = operationsMapper.toDTO(operand1, Operator.SUB, operand2);
+        Assertions.assertTrue(operation instanceof Subtraction);
+        Assertions.assertEquals(((Subtraction) operation).getOperand1(), operand1);
+        Assertions.assertEquals(((Subtraction) operation).getOperand2(), operand2);
     }
 }
